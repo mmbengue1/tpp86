be4817074a72a6e0cd47e6ecf7be776e5b82c0d2
mmbengue <kingmbengue@gmail.com>
2015-12-02 11:09:06 -0500

    123 push push it to the limit pupush

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /**
  * Created by ddubreui on 2015-11-26.
  */
-
+var chargement;
 var score = 0;
 var scoretext;
 var time = 0;
@@ -14,80 +14,46 @@ var targetXSpeed = 1.5;
 var targetYSpeed = 1.75;
 WIDTH = 1000;
 HEIGHT = 1000;
-function keepTargetFitted()
-{
 
-    if(targetXPos < WIDTH && targetXPos > 0)
-    {
-        targetXPos += targetXSpeed;
-    } else
-    {
-        targetXSpeed = targetXSpeed * (-1);
-        targetXPos +=targetXSpeed;
-    }
-    if(targetYPos < HEIGHT && targetYPos > 0)
-    {
-        targetYPos += targetYSpeed;
-    } else
-    {
-        targetYSpeed = targetYSpeed * (-1);
-        targetYPos += targetYSpeed;
-    }
-
-    animation.x = targetXPos;
-    animation.y = targetYPos;
-
-
-}
 /*
-var target = new Object();
-target.element = 'target';
-target.y = 250;
-target.x = 450;
-
-
-document.getElementById("container").addEventListener('click' , function() {
-    var innerDiv = document.getElementById("target");
-    console.log(innerDiv.style.left.length);
-    if (innerDiv.style.left == "") {
-        innerDiv.style.left = "0px"
-    };
-    var posX = (parseInt(innerDiv.style.left, 10) + 20) + "px";
-    innerDiv.style.left = posX;
-    console.log(posX);
+timerText = new createjs.Text("Time: " + gameTime.toString(), "36px Arial", "#FFF");
+timerText.x = 800;
+timerText.y = 10;
+stage.addChild(timerText);
 */
 
-
-var imgObj = null;
-var animate ;
-
-function init(){
-    imgObj = document.getElementById('spirite');
-    imgObj.style.position= 'relative';
-    imgObj.style.left = '0px';
-}
-
-function moveRight(){
-    imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
-    animate = setTimeout(moveRight,20); // call moveRight in 20msec
-}
-
-function moveTop(){
-    imgObj.style.bottom = parseInt(imgObj.style.bottom) + 10 + 'px';
-    animate = setTimeout(moveTop,20); // call moveRight in 20msec
-}
-
-function stop(){
-    clearTimeout(animate);
-    imgObj.style.left = '0px';
-}
-
-window.onload =init;
-
-//Animation de l'image on click
+//permutation de l'image on click
 if(document.images){
-    var image1 = new Image(); // Preload an image
+    var image1 = new Image(); // charge une image a l'avance
     image1.src = "apparence/images/Isi2.jpg";
-    var image2 = new Image(); // Preload second image
+    var image2 = new Image(); //charger une deuxieme image a l'avance
     image2.src = "apparence/images/Star-Wars-Ship-Posters-2.gif";
 }
+
+//fonction pour animer une image aleatoire avec un math random
+$(document).ready(function(){
+    animateSpirit();
+
+});
+//Generer une position aleatoire parraport a la fenetre du navigateur
+function randomPosition(){
+
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width()  - 50;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh,nw];
+
+}
+//animation de l'image en passant par l'id qui lui est attribue
+function animateSpirit(){
+    var newq = randomPosition();
+    $('#spirite').animate({ top: newq[0], left: newq[1] }, function(){
+        animateSpirit();
+    });
+
+};
+
