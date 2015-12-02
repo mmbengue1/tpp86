9555f30d123399f528327c017ad4a61daf30e31d
mmbengue <kingmbengue@gmail.com>
2015-12-02 14:15:27 -0500

    commit and push

--- js/main.js
+++ js/main.js
@@ -20,14 +20,25 @@ document.getElementById('spirite').onclick=function(){
     var score = parseInt(document.getElementById("score").innerHTML);
     score = score + 50;//incrementaion du scrore par 50 a chaque click
     document.getElementById("score").innerHTML = score;
-}
 
+}
+//faire disparaitre  et reapparaitre une image au click
+
+$(document).ready(function(){
+    $("#spirite").click(function() {
+        $("#spirite").fadeOut(1000);
+    });
+
+    $("#spirite").click(function() {
+        $("#spirite").fadeIn(1500);
+    });
+});
 //permutation de l'image on click
 if(document.images){
     var image1 = new Image(); // charge une image a l'avance
-    image1.src = "apparence/images/Isi2.jpg";
+    image1.src = "apparence/images/Star-Wars-Ship-1.gif";
     var image2 = new Image(); //charger une deuxieme image a l'avance
-    image2.src = "apparence/images/Star-Wars-Ship-Posters-2.gif";
+    image2.src = "apparence/images/Star-Wars-Ship-Posters-2.gif";//l'image
 }
 
 //fonction pour animer un objet aleatoire avec un math random
@@ -78,3 +89,24 @@ function calcSpeed(prev, next) {
     return speed;
 
 }
+var timer;
+var output;
+var game;
+
+function init(){
+    game = new Scene();
+    output = document.getElementById("output");
+    timer = new Timer();
+    timer.reset();
+    game.start();
+} // end init
+
+function update(){
+    game.hide();
+    currentTime = timer.getElapsedTime();
+    output.innerHTML = currentTime;
+} // end update
+
+function reset(){
+    timer.reset();
+} // end reset
